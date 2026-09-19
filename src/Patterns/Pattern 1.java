ecc3bed545af7c67b4ccc1893620536a5c313861
ams21-Dev <amsingh720660@gmail.com>
2026-09-19 14:20:38 +0530

    update structure of folder

--- src/Patterns/Pattern 1.java
+++ src/Patterns/Pattern 1.java
@@ -1,5 +1,5 @@
-public class Main {
-    public static void main(String[] args) {
+class Pattern1 {
+        public static void main(String[] args) {
     int i;
     for  (i = 0; i < 5; i++) {
         for(int j = 0; j < 5; j++) {
@@ -9,4 +9,5 @@ public class Main {
     }
 
     }
+    
 }
